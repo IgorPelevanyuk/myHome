7f85c23863d85cfad216e58998e900053bdd7d62
IgorPelevanyuk <gavelock@gmail.com>
2018-03-21 18:22:04 +0300

    UPDATE: Highcharts added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { ChartModule } from 'angular2-highcharts';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ValueComponent } from './value/value.component';
 import { RestService } from './rest.service';
+import { PlotComponent } from './plot/plot.component';
 
+declare var require: any;
+export function highchartsFactory() {
+  return require('highcharts');
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    ValueComponent
+    ValueComponent,
+    PlotComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ChartModule
   ],
-  providers: [RestService],
+  providers: [
+    RestService,
+    {
+      provide: HighchartsStatic,
+      useFactory: highchartsFactory
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/plot/plot.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { RestService } from '../rest.service';
+
+import { Timeval } from '../timeval';
+
+@Component({
+  selector: 'app-plot',
+  templateUrl: './plot.component.html',
+  styleUrls: ['./plot.component.css'],
+  inputs: ['table']
+})
+
+export class PlotComponent implements OnInit {
+  @Input() table: string;
+  private values: Timeval[];
+  options: Object;
+  chart: Object;
+
+
+  constructor(private restService: RestService,) {
+    this.values = [];
+  }
+
+  saveInstance(chartInstance): void {
+    this.chart = chartInstance;
+  }
+
+  initChart(values): void {
+    this.options = {
+      chart: { zoomType: 'x' },
+      title : { text : this.table },
+      xAxis: { type: 'datetime' },
+      series: [{
+        type: 'area',
+        data: values,
+      }],
+      plotOptions: {
+        area: {
+          marker: { radius: 2 },
+          lineWidth: 1,
+          states: {
+            hover: { lineWidth: 1 }
+          },
+          threshold: null
+        }
+      }
+    };
+  }
+
+  getValues() {
+    this.restService.getTableData(this.table).subscribe(
+      //values => this.values = values,
+      values => this.initChart(values)
+    );
+  }
+
+  ngOnInit() {
+    this.getValues();
+
+  }
+
+}
