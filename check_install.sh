e551e76ae342e1dea7e7e70211eebfbfefb702fa
IgorPelevanyuk <gavelock@gmail.com>
2021-01-08 17:15:24 +0300

    UPDATE: expand checking consistensy in code

--- check_install.sh
+++ check_install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+echo "Check radio_listner.py"
 diff ./radio_listner/radio_listner.py /opt/radio_listner.py
+echo "Check radio_listner.service"
 diff ./radio_listner/radio_listner.service /etc/systemd/system/radio_listner.service
+
+echo "Check rpi_reporter.py"
+diff ./rpi_reporter/rpi_reporter.py /opt/rpi_reporter.py
+echo "Check rpi_reporter.service"
+diff ./rpi_reporter/rpi_reporter.service /etc/systemd/system/rpi_reporter.service
 
